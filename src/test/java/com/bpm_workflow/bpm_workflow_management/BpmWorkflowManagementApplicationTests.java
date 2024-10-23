8fb83b977b2de08edf41a4c9dc58ffc69e9b7616
Sachira Karunarathna <sachira@focalid.tech>
2024-10-23 12:48:31 +0530

    Initial commit

---
+++ src/test/java/com/bpm_workflow/bpm_workflow_management/BpmWorkflowManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpm_workflow.bpm_workflow_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmWorkflowManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
