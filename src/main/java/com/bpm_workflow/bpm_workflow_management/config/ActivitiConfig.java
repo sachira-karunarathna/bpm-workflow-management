c2c1d2fa84c770eeb22f4d8e40477972d4910997
Sachira Karunarathna <sachira@focalid.tech>
2024-10-23 17:00:17 +0530

    Added environment variables

--- src/main/java/com/bpm_workflow/bpm_workflow_management/config/ActivitiConfig.java
+++ src/main/java/com/bpm_workflow/bpm_workflow_management/config/ActivitiConfig.java
@@ -1,7 +1,57 @@
 package com.bpm_workflow.bpm_workflow_management.config;
 
+import org.activiti.engine.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ActivitiConfig {
+
+    @Value("${ACTIVITI_DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
+
+    @Bean
+    public ProcessEngine processEngine(ProcessEngineConfiguration processEngineConfiguration) {
+        return processEngineConfiguration.buildProcessEngine();
+    }
+
+    @Bean
+    public RuntimeService runtimeService(ProcessEngine processEngine) {
+        return processEngine.getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService(ProcessEngine processEngine) {
+        return processEngine.getTaskService();
+    }
+
+    @Bean
+    public RepositoryService repositoryService(ProcessEngine processEngine) {
+        return processEngine.getRepositoryService();
+    }
+
+    @Bean
+    public HistoryService historyService(ProcessEngine processEngine) {
+        return processEngine.getHistoryService();
+    }
+
+    @Bean
+    public ProcessEngineConfiguration processEngineConfiguration() {
+        return ProcessEngineConfiguration
+                .createStandaloneProcessEngineConfiguration()
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+                .setJdbcUrl(dbUrl)
+                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
+                .setJdbcUsername(dbUsername)
+                .setJdbcPassword(dbPassword)
+                .setAsyncExecutorActivate(false);
+    }
+
 }
